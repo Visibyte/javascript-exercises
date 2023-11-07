c6b1f9f57615f9846c84152041b517c2c960a70e
Visibyte <nathanglass26@gmail.com>
2023-11-07 14:19:52 +0000

    palindromes solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let forward = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reverse = forward.split("").reverse().join("");
 
+  if (forward === reverse) {
+    return true;
+  }
+
+  return false;
 };
 
 // Do not edit below this line
