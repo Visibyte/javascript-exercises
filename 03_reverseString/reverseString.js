4a497d8c1eae2179c268f582f07bad1b36da887d
Visibyte <nathanglass26@gmail.com>
2023-11-07 12:14:35 +0000

    reverse string solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  if (string === "") {
+    return "";
+  }
 
+  for (let i = 0; i < string.length; i++) {
+    let result = string.split("").reverse().join("");
+
+    return result;
+  }
 };
 
 // Do not edit below this line
