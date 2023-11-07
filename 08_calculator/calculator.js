bf3fc8ac32dd23349973930901231fc93b28e30f
Visibyte <nathanglass26@gmail.com>
2023-11-07 13:56:25 +0000

    calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((currentNum, add) => add + currentNum, 0);
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  let sum = 1;
+  for (let i = 0; i < arr.length; i++) {
+    sum *= arr[i];
+  }
 
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let product = 1;
+  for (let i = num; i > 0; i--) {
+    product *= i;
+  }
+
+  return product;
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
