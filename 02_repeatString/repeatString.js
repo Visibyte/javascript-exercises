2528c53348fbdd40757dcaad30ae85f9f5c258e7
Visibyte <nathanglass26@gmail.com>
2023-11-07 11:39:27 +0000

    repeat string challenge complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let stringToReturn = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    stringToReturn = stringToReturn.concat(string);
+  }
+
+  return stringToReturn;
 };
 
 // Do not edit below this line
